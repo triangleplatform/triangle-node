08cbfeda6824216ad797add29e3fc74d6d613bb2
tasti <tasti@zakarie.com>
2022-12-13 18:33:11 -0500

    Add params for chains and networks

--- src/resources/chains.ts
+++ src/resources/chains.ts
@@ -1,11 +1,14 @@
 import Api from "../utils/Api";
 import { Config } from "../types";
 
+export interface ChainListParams {}
+export interface ChainRetrieveParams {}
+
 const chains = (config: Config) => {
   const api = new Api(config);
   return {
-    list: () => api.get(`/chains`),
-    retrieve: (id: string) => api.get(`/chains/${id}`),
+    list: (params?: ChainListParams) => api.get(`/chains`, params),
+    retrieve: (id: string, params?: ChainRetrieveParams) => api.get(`/chains/${id}`, params),
   };
 };
 
--- src/resources/networks.ts
+++ src/resources/networks.ts
@@ -1,11 +1,14 @@
 import Api from "../utils/Api";
-import { Config } from "../types";
+import { Config, ExpandParams } from "../types";
+
+export interface NetworkListParams extends ExpandParams {}
+export interface NetworkRetrieveParams extends ExpandParams {}
 
 const networks = (config: Config) => {
   const api = new Api(config);
   return {
-    list: () => api.get(`/networks`),
-    retrieve: (id: string) => api.get(`/networks/${id}`),
+    list: (params?: NetworkListParams) => api.get(`/networks`, params),
+    retrieve: (id: string, params?: NetworkRetrieveParams) => api.get(`/networks/${id}`, params),
   };
 };
 
